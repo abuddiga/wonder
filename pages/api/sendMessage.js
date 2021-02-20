5b1592cabfb89a20824bac200a310ed78644a489
Akshay Buddiga <abuddiga@fanatics.com>
2021-02-20 11:48:32 -0800

    Send multiple texts

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -1,10 +1,10 @@
 import { accountSid, authToken, fromPh } from '../../fetchData/triggerSms'
 
-export default (req, res) => {
-  const toPh = req.body.toPh
+export default async (req, res) => {
+  const toPhs = req.body.toPhs
   const activityMatch = req.body.activityMatch
 
-  console.log('toPh: ', toPh)
+  console.log('toPh: ', toPhs)
   console.log('activityMatch: ', activityMatch)
 
   console.log('accountSid', accountSid)
@@ -12,16 +12,40 @@ export default (req, res) => {
 
   const client = require('twilio')(accountSid, authToken);
 
-  client.messages
-    .create({
-      body: `${activityMatch.title}. ${activityMatch.description}`,
-      mediaUrl: activityMatch.img_link,
-      from: fromPh,
-      to: toPh
+  const results = await Promise.all(
+    toPhs.map(async toPh => {
+      return client.messages
+      .create({
+        body: `${activityMatch.title}. ${activityMatch.description}`,
+        mediaUrl: activityMatch.img_link,
+        from: fromPh,
+        to: toPh
+      })
+      .then(message => {
+        console.log('message: ', message.sid)
+        return null
+      })
+      .catch(err => {
+        console.log('error: ', err)
+        return err
+      })
     })
-    .then(message => console.log(message.sid))
-    .catch(err => console.log(err))
+  )
 
-  res.statusCode = 200
-  res.json({ ok: true })
+  const ok = results.reduce((acc, curr) => {
+    return acc && curr === null
+  }, true)
+
+  console.log('results: ', results)
+  console.log('ok: ', ok)
+
+  if (ok) {
+    res.statusCode = 200
+    res.json({ ok: true })
+  } else {
+    res.statusCode = 500
+    res.json({
+      err: results
+    })
+  }
 }
