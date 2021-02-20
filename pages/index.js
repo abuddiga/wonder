b60044137b5e7be9d1402e1549e519c537062d65
Eric Luttmann <elutt3@gmail.com>
2021-02-20 09:15:27 -0800

    20200221_stylesupdate

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { Button } from 'evergreen-ui'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Image alt="Kick it logo" src="/logo.png" width={477} height={318} />
+        <h3 className={styles.title}>
           Welcome to Kick It
-        </h1>
+          
+        </h3>
         <div className={styles.grid}>
           <Link href="/start" className={styles.card}>
             <Button>Start a Session</Button>
